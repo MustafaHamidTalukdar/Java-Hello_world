8fca3ee00760eae2644281183c8da9e123544743
Mustafa Hamid Talukdar <68769724+MustafaHamidTalukdar@users.noreply.github.com>
2020-12-28 16:47:54 +0530

    Add files via upload

---
+++ Java_01.java
@@ -0,0 +1,13 @@
+/***********************************
+ ***************@MUSTAFA************
+ **********************************/
+
+package com.company;
+
+public class Java_01 {
+
+    public static void main(String[] args) {
+        System.out.print("I am Mustafa and ");
+        System.out.println("I am 13 years old");
+    }
+}
